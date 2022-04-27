4f7c916c6b8390a59834d9356710a9f1dcb1fb98
alejandrorey96 <82549214+alejandrorey96@users.noreply.github.com>
2022-04-27 10:56:46 +0200

    GameLore
    Proyecto para Programación Multimedia y Dispositivos Móviles.
    (Re-hecho de 0 por problemas técnicos)

--- android/app/src/main/java/com/example/menuabout/MainActivity.java
+++ android/app/src/main/java/com/example/menuabout/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.menuabout;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +23,25 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about:
+                AlertDialog.Builder alert = new AlertDialog.Builder(this);
+                alert.setMessage("Dialogo About");
+                alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        System.out.println("test");
+                    }
+                });
+                AlertDialog dialog = alert.create();
+                dialog.show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
