6b65e89087de0c9071d6c5a490afcbeeac65d718
alejandrorey96 <alexrey2a@gmail.com>
2022-04-29 14:47:43 +0200

    Volley data collection working.
    The data from categories.json will appear in the screen on MainActivity.

--- GameLore/app/src/main/java/com/example/gamelore/MainActivity.java
+++ GameLore/app/src/main/java/com/example/gamelore/MainActivity.java
@@ -3,12 +3,64 @@ package com.example.gamelore;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String url = "https://raw.githubusercontent.com/alejandrorey96/GameLore/master/GameLore/data/categories.json";
+    private TextView mTextViewResult;
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextViewResult = findViewById(R.id.text_view_result);
+        mQueue = Volley.newRequestQueue(this);
+
+
+        JsonObjectRequest categoryRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("categories");
+                            for(int i = 0; i<jsonArray.length();i++) {
+
+                                JSONObject category = jsonArray.getJSONObject(i);
+                                String name = category.getString("name");
+                                String id = category.getString("id");
+
+                                mTextViewResult.append(name + "\n" + id + "\n\n");
+                            }
+                        }catch (JSONException e){
+                                e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                }
+        );
+        mQueue.add(categoryRequest);
     }
 }
