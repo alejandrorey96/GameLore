4a728af24c1c284c9ae55be4531d58da1d6e40c3
alejandrorey96 <alexrey2a@gmail.com>
2022-05-16 23:04:39 +0200

    Uploaded completeCategory for good.
    CompleteCategory didn't got attached to the commit before.

---
+++ GameLore/app/src/main/java/com/example/gamelore/CompleteCategory.java
@@ -0,0 +1,58 @@
+package com.example.gamelore;
+
+import androidx.annotation.Nullable;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class CompleteCategory {
+    private String id;
+    private String name;
+    private String description;
+    private Boolean is_locked;
+    private String image_url;
+    private String character;
+    private String arena;
+
+    public CompleteCategory(JSONObject objectJason) throws JSONException {
+        this.id = objectJason.getString("id");
+        this.name = objectJason.getString("name");
+        this.description = objectJason.getString("description");
+        this.is_locked = objectJason.getBoolean("is_locked");
+        this.image_url = objectJason.getString("image_url");
+        this.character = objectJason.getString("character");
+        this.arena = objectJason.getString("arena");
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Nullable
+    public String getDescription() {
+        return description;
+    }
+
+    @Nullable
+    public Boolean getIs_locked() {
+        return is_locked;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    @Nullable
+    public String getCharacter() {
+        return character;
+    }
+
+    @Nullable
+    public String getArena() {
+        return arena;
+    }
+}
